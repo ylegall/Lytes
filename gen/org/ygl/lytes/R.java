c9348ce8669bea32b9537bd1506a425269ffa3ef
Yann G. Le Gall <ylegall@gmail.com>
2010-06-08 00:39:12 -0400

    minor layout changes

--- gen/org/ygl/lytes/R.java
+++ gen/org/ygl/lytes/R.java
@@ -11,12 +11,13 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int hex_off=0x7f020000;
-        public static final int hex_on=0x7f020001;
-        public static final int icon=0x7f020002;
-        public static final int lytes=0x7f020003;
-        public static final int off=0x7f020004;
-        public static final int on=0x7f020005;
+        public static final int but=0x7f020000;
+        public static final int hex_off=0x7f020001;
+        public static final int hex_on=0x7f020002;
+        public static final int icon=0x7f020003;
+        public static final int lytes=0x7f020004;
+        public static final int off=0x7f020005;
+        public static final int on=0x7f020006;
     }
     public static final class id {
         public static final int LOGO=0x7f050006;
